aa18a01a957e3727b9a81eeb865bdfce9dd70caa
Martin Reinhardt <hypery2k@web.de>
2011-11-14 08:54:41 +0100

    project setup

---
+++ Inventory-DB Server/src/main/java/model/Media.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Media {
+}
